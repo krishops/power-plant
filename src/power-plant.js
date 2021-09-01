3448be897ef34601399a2143322acc7fe3f8dd7c
Kris Hopper <hopperdavis@gmail.com>
2021-09-01 09:04:51 -0700

    practice functional programming

---
+++ src/power-plant.js
@@ -0,0 +1,24 @@
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+const blueFood = changeState("soil")(5)
+const greenFood = changeState("soil")(10)
+const yuckyFood = changeState("soil")(-5)
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
